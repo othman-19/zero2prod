27e599580391e64074a857275c85372f1b4db91e
othman-19 <othmanenaamani@gmail.com>
2023-06-12 19:41:24 +0100

    installed dependencies, created first migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,21 @@
 
 set -x
 set -eo pipefail
+
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
@@ -19,3 +34,19 @@ docker run \
   -d postgres \
   postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
+
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1 
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
+
+ # Assuming you used the default parameters to launch Postgres in Docker!
+# export DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
+sqlx migrate add create_subscriptions_table
