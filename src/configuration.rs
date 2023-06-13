c8eeecf9dbfe7353ba36697d4cc9a9af9b2dbc78
othman-19 <othmanenaamani@gmail.com>
2023-06-13 15:35:06 +0100

    Added configuration.yaml file, get_configuration function, get configuration into main.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,3 +12,15 @@ pub struct DatabaseSettings {
   pub host: String,
   pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> { 
+  // Initialise our configuration reader
+  let mut settings = config::Config::default();
+  // Add configuration values from a file named `configuration`.
+  // It will look for any top-level file with an extension
+  // that `config` knows how to parse: yaml, json, etc.
+  settings.merge(config::File::with_name("configuration"))?;
+  // Try to convert the configuration values it read into
+  // our Settings type
+  settings.try_into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use zero2prod::startup::run;
 use std::net::TcpListener;
-
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
