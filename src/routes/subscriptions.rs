ccc7be3e643d8281212bfdbf5b82dc60d2bf0acb
othman-19 <othmanenaamani@gmail.com>
2023-06-21 19:21:22 +0100

    added logs to the  handler

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,18 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    // Let's generate a random unique identifier
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name,
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#" INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
@@ -22,13 +34,23 @@ pub async fn subscribe(
     )
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    .await 
+        .execute(pool.get_ref())
+        .await 
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id,
+            );
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-          println!("Failed to execute query: {}", e);
-          HttpResponse::InternalServerError().finish()
-        }
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e,
+            );
+            HttpResponse::InternalServerError().finish()
+        },
     }
 }
