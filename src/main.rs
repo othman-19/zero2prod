27bb879c171bd57990fb30c8be5eda8739eff800
othman-19 <othmanenaamani@gmail.com>
2023-06-21 18:27:04 +0100

    added a loger to the app

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, App, HttpServer};
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
-
-use crate::routes::{health_check, subscribe};
 
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
@@ -12,9 +13,11 @@ pub fn run(
   listener: TcpListener,
   db_pool: PgPool,
 ) -> Result<Server, std::io::Error> {
-  let db_pool = web::Data::new(db_pool);
+  let db_pool = Data::new(db_pool);
   let server = HttpServer::new(move || {
       App::new()
+          // Middlewares are added using the `wrap` method on `App`
+          .wrap(Logger::default())
           .route("/health_check", web::get().to(health_check))
           .route("/subscriptions", web::post().to(subscribe))
           // Register the connection as part of the application state
